6da32754796f6a06f2b469dbed3a6ff7c6731cb9
Emad Elsaid <blazeeboy@gmail.com>
2015-09-11 15:38:26 +0200

    google analytics

--- app/router.js
+++ app/router.js
@@ -2,7 +2,13 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
+  notifyGoogleAnalytics: function() {
+    return ga('send', 'pageview', {
+        'page': this.get('url'),
+        'title': this.get('url')
+      });
+  }.on('didTransition')
 });
 
 Router.map(function() {
--- config/environment.js
+++ config/environment.js
@@ -8,10 +8,10 @@ module.exports = function(environment) {
     locationType: 'auto',
     contentSecurityPolicy: {
       'default-src': "'none'",
-      'script-src': "'self' 'unsafe-inline' 'unsafe-eval' use.typekit.net connect.facebook.net maps.googleapis.com maps.gstatic.com",
+      'script-src': "'self' 'unsafe-inline' 'unsafe-eval' www.google-analytics.com use.typekit.net connect.facebook.net maps.googleapis.com maps.gstatic.com",
       'font-src': "'self' data: use.typekit.net",
       'connect-src': "'self'",
-      'img-src': "'self' www.facebook.com p.typekit.net",
+      'img-src': "'self' www.facebook.com p.typekit.net www.google-analytics.com",
       'style-src': "'self' 'unsafe-inline' use.typekit.net",
       'frame-src': "s-static.ak.facebook.com static.ak.facebook.com www.facebook.com"
     },
