49a3e19a22960803f60a134696739416253a213c
Emad Elsaid <blazeeboy@gmail.com>
2015-12-10 22:24:37 +0200

    #5 add togetherjs

--- config/environment.js
+++ config/environment.js
@@ -8,12 +8,13 @@ module.exports = function(environment) {
     locationType: 'hash',
     contentSecurityPolicy: {
       'default-src': "'none'",
-      'script-src': "'self' 'unsafe-inline' 'unsafe-eval' www.google-analytics.com use.typekit.net connect.facebook.net maps.googleapis.com maps.gstatic.com",
-      'font-src': "'self' data: use.typekit.net",
-      'connect-src': "'self'",
-      'img-src': "'self' www.facebook.com p.typekit.net www.google-analytics.com",
-      'style-src': "'self' 'unsafe-inline' use.typekit.net",
-      'frame-src': "'self' s-static.ak.facebook.com static.ak.facebook.com www.facebook.com"
+      'script-src': "'self' 'unsafe-inline' 'unsafe-eval' www.google-analytics.com use.typekit.net connect.facebook.net maps.googleapis.com maps.gstatic.com *.togetherjs.com togetherjs.com",
+      'font-src': "'self' data: use.typekit.net *.togetherjs.com togetherjs.com",
+      'connect-src': "'self' *.togetherjs.com togetherjs.com *.ytimg.com",
+      'img-src': "'self' www.facebook.com p.typekit.net www.google-analytics.com *.togetherjs.com togetherjs.com",
+      'style-src': "'self' 'unsafe-inline' use.typekit.net *.togetherjs.com togetherjs.com",
+      'frame-src': "'self' s-static.ak.facebook.com static.ak.facebook.com www.facebook.com *.togetherjs.com togetherjs.com docs.google.com",
+      "media-src": "'self' togetherjs.com *.togetherjs.com"
     },
     EmberENV: {
       FEATURES: {
