f087622394f4c1222e297485305a2b0a3cdcbafb
Emad Elsaid <blazeeboy@gmail.com>
2015-12-19 19:10:32 +0200

    #8 add copy, cut actions to textarea component

--- app/components/devtools-textarea.js
+++ app/components/devtools-textarea.js
@@ -1,7 +1,34 @@
 import Ember from 'ember';
+import Clipboard from 'npm:clipboard';
 
 export default Ember.Component.extend({
   classNames: ['devtools-textarea'],
+
+  didInsertElement: function(){
+    var view = this,
+        clipboard_copy = new Clipboard(this.$('.copy-btn')[0], {
+          text: function(){
+            return view.get('value');
+          }
+        }),
+        clipboard_cut = new Clipboard(this.$('.cut-btn')[0], {
+          text: function(){
+            return view.get('value');
+          }
+        }).on('success', function(){
+          view.set('value', '');
+        });
+        
+    this.setProperties({
+      'clipboard_copy': clipboard_copy,
+      'clipboard_cut': clipboard_cut
+    });
+  },
+
+  willDestroyElement: function(){
+    this.get('clipboard_copy').destroy();
+    this.get('clipboard_cut').destroy();
+  },
 
   actions: {
     selectAll: function(){
