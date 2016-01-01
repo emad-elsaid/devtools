42e7a916ca823cc352a8a0da77682507e97c650a
Emad Elsaid <blazeeboy@gmail.com>
2016-01-01 04:20:38 +0200

    enhance topper design as suggested in e401e8

--- app/components/app-navbar.js
+++ app/components/app-navbar.js
@@ -3,10 +3,12 @@ import FoundationBase from '../mixins/foundation-base';
 
 export default Ember.Component.extend(FoundationBase, {
   classNames: ['fixed'],
-  routesService: Ember.inject.service('routes'),
   actions: {
     toggleSidebar: function(){
       this.toggleProperty('sidebar');
     }
-  }
+  },
+  showFullLogo: function(){
+    return this.currentPath === 'index';
+  }.property('currentPath')
 });
