2896721bac3c80dcffad3a4e3b8f6f93a51d11fa
Emad Elsaid <blazeeboy@gmail.com>
2016-01-02 08:48:14 +0200

    add tool name as document title

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
+import { titlize } from '../helpers/titlize';
 
 export default Ember.Route.extend({
+  routesService: Ember.inject.service('routes'),
+
+  title: function(tokens) {
+    if(tokens.length > 0){
+      return tokens.join(' - ') + ' - DevTools';
+    }else{
+      var currentRoute = this.controller.currentRouteName;
+      if(this.get('routesService').get('routes').includes(currentRoute)){
+        return `${titlize([currentRoute])} - DevTools`;
+      }else{
+        return 'DevTools';
+      }
+    }
+  }
 });
